f7f19ad604ec3c0d40c6a9036f4a80900c1b6a04
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2024-02-20 00:55:05 +1100

    first homework exercise complete

---
+++ sim8086/sim8086.c
@@ -0,0 +1,143 @@
+#include "sim8086.h"
+
+void disassemble(u8 buffer[], u32 idx);
+Instruction get_next_instruction(u8 buffer[], u32 n);
+void print_instruction(Instruction* inst, FILE* dest);
+void decode_rm_reg(Instruction* inst, u8 buffer[]);
+Operand get_reg_operand(u8 register_idx, u8 wide);
+void print_operand(Operand operand, FILE* dest);
+
+int main(int argc, char *argv[]) {
+    // make sure at least one machine code file was provided
+    if (argc < 2) {
+        fprintf(stderr, "USAGE: %s [8086 machine code file] ...\n", argv[0]);
+        exit(1);
+    }
+
+    u8 buffer[BUFFER_SIZE]; // buffer for holding raw bytes of 8086 machine code
+
+    for (int i = 1; i < argc; i++) {
+        FILE *fp;
+        if ((fp = fopen(argv[1], "rb")) == NULL) {
+            fprintf(stderr, "ERROR: unable to open %s\n", argv[1]);
+            exit(1);
+        };
+
+        u32 bytes_read = fread(buffer, sizeof(u8), BUFFER_SIZE, fp);
+        fclose(fp);
+
+        disassemble(buffer, bytes_read);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+void disassemble(u8 buffer[], u32 n) {
+    u32 i = 0;
+    while (i < n) {
+        Instruction inst = get_next_instruction(buffer, i);
+        if (i + inst.size <= n) {
+            i += inst.size;
+        } else {
+            fprintf(stderr, "ERROR: instruction exceeds disassembly region.\n");
+            exit(1);
+        }
+        print_instruction(&inst, stdout);
+    }
+}
+
+Instruction get_next_instruction(u8 buffer[], u32 idx) {
+    Instruction inst;
+    inst.address = idx;
+
+    u8 opcode = buffer[inst.address];
+    opcode >>= 2;
+    switch (opcode) {
+        case 0b100010:
+            inst.op = OpMov;
+            decode_rm_reg(&inst, buffer);
+            break;
+        default:
+            fprintf(stderr, "ERROR: unknown opcode encountered.\n");
+            exit(1);
+    }
+
+    return inst;
+}
+
+void decode_rm_reg(Instruction* inst, u8 buffer[]) {
+    u32 idx = inst->address;
+    u8 d, w, mod, reg, rm;
+    d = (buffer[idx] >> 1) & 1;
+    w = buffer[idx] & 1;
+    idx++;
+    mod = buffer[idx] >> 6;
+    reg = (buffer[idx] >> 3) & 0b111;
+    rm = buffer[idx] & 0b111;
+    if (mod == 0b11) {  // register mode
+        inst->operands[d] = get_reg_operand(rm, w);
+    }
+    inst->operands[d ? 0 : 1] = get_reg_operand(reg, w);
+    inst->size = idx - inst->address + 1;
+}
+
+Operand get_reg_operand(u8 register_idx, u8 wide) {
+    RegisterAccess reg_table[][2] = {
+        {{Reg_a, 0, 1}, {Reg_a, 0, 2}},
+        {{Reg_c, 0, 1}, {Reg_c, 0, 2}},
+        {{Reg_d, 0, 1}, {Reg_d, 0, 2}},
+        {{Reg_b, 0, 1}, {Reg_b, 0, 2}},
+        {{Reg_a, 1, 1}, {Reg_sp, 0, 2}},
+        {{Reg_c, 1, 1}, {Reg_bp, 0, 2}},
+        {{Reg_d, 1, 1}, {Reg_si, 0, 2}},
+        {{Reg_b, 1, 1}, {Reg_di, 0, 2}},
+    };
+    Operand res;
+    res.kind = OperandRegister;
+    res.reg = reg_table[register_idx][wide];
+    return res;
+}
+
+char* get_mnemonic(u32 idx) {
+    char *mnemonics[] = {
+            "",
+            "mov",
+    };
+    return mnemonics[idx];
+}
+
+void print_instruction(Instruction* inst, FILE *dest) {
+    fprintf(dest, "%s ", get_mnemonic(inst->op));
+    print_operand(inst->operands[0], dest);
+    fprintf(dest, ", ");
+    print_operand(inst->operands[1], dest);
+    fprintf(dest, "\n");
+}
+
+char* get_reg_name(u32 reg_idx, u32 part_idx) {
+    char *reg_names[][3] = {
+            {"",   "",   ""},
+            {"al", "ah", "ax"},
+            {"bl", "bh", "bx"},
+            {"cl", "ch", "cx"},
+            {"dl", "dh", "dx"},
+            {"sp", "sp", "sp"},
+            {"bp", "bp", "bp"},
+            {"si", "si", "si"},
+            {"di", "di", "di"},
+    };
+    return reg_names[reg_idx][part_idx];
+}
+
+void print_operand(Operand operand, FILE* dest) {
+    switch (operand.kind) {
+        case OperandRegister: {
+            RegisterAccess reg = operand.reg;
+            fprintf(dest, "%s", get_reg_name(reg.index, (reg.count == 2) ? 2 : reg.offset));
+            break;
+        }
+        default:
+            fprintf(stderr, "ERROR: unknown operand encountered.\n");
+            exit(1);
+    };
+}
---
+++ sim8086/sim8086.h
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+#define len(arr) (sizeof(arr) / sizeof(arr[0]))
+
+#define BUFFER_SIZE 1024*1024
+
+typedef enum {
+    OpNone,
+
+    OpMov,
+
+    OpCount,
+} OpType;
+
+typedef enum {
+    Reg_none,
+
+    Reg_a,
+    Reg_b,
+    Reg_c,
+    Reg_d,
+    Reg_sp,
+    Reg_bp,
+    Reg_si,
+    Reg_di,
+    Reg_es,
+    Reg_cs,
+    Reg_ss,
+    Reg_ds,
+    Reg_ip,
+
+    Reg_count,
+} Register;
+
+typedef enum {
+    Ea_direct,
+
+    Ea_bx_si,
+    Ea_bx_di,
+    Ea_bp_si,
+    Ea_bp_di,
+    Ea_si,
+    Ea_di,
+    Ea_bp,
+    Ea_bx,
+
+    Ea_count,
+} EffectiveAddressBase;
+
+typedef struct {
+    Register segment;
+    EffectiveAddressBase base;
+    i32 displacement;
+} EffectiveAddress;
+
+typedef struct {
+    Register index;
+    u8 offset;
+    u8 count;
+} RegisterAccess;
+
+typedef enum {
+    OperandRegister,
+    OperandMemory,
+    OperandImmediate,
+} OperandType;
+
+typedef struct {
+    OperandType kind;
+    union {
+        RegisterAccess reg;
+        EffectiveAddress address;
+    };
+} Operand;
+
+typedef struct {
+    u32 address;            // relative address in buffer
+    u32 size;               // size of instruction in bytes
+    OpType op;              // opcode type
+    Operand operands[2];    // instruction operands
+} Instruction;
