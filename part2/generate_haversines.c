6eb846019bba527ce81f3e2a72a98b039889d719
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2024-02-26 20:45:18 +1100

    got cluster mode working for coordinate generation

--- part2/generate_haversines.c
+++ part2/generate_haversines.c
@@ -1,18 +1,36 @@
 #include "haversine.c"
 
-bool cluster = false;
 u64 seed = 0;
 u64 pair_count = 0;
 
-f64 random_f64(i32 min, i32 max) {
+f64 random_f64(f64 min, f64 max) {
     assert(max - min <= RAND_MAX);
     f64 normalised_rand = rand() / (f64)RAND_MAX;
-    u32 diff = abs(max - min);
+    f64 diff = abs((i32)max - (i32)min);
     f64 res = min + (normalised_rand * diff);
     return res;
 }
 
+f64 random_degree(f64 centre, f64 radius) {
+    f64 upper = centre + radius;
+    f64 lower = -upper;
+
+    upper = min(upper, X_MAX);
+    lower = max(lower, -X_MAX);
+
+    f64 res = random_f64(upper, lower);
+    return res;
+}
+
 int main(int argc, char *argv[]) {
+    f64 x_centre = 0;
+    f64 y_centre = 0;
+    f64 x_radius = X_MAX;
+    f64 y_radius = Y_MAX;
+
+    // for counting how many more coordinates to generate in the current cluster
+    u64 cluster_count = UINT64_MAX;
+
     if (argc != 4) {
         fprintf(stderr, "USAGE: %s [uniform/cluster] [seed] [number of coordinates to generate]\n", argv[0]);
         exit(1);
@@ -20,54 +38,66 @@ int main(int argc, char *argv[]) {
 
     // parse mode of coordinate generation
     if (strcmp(argv[1], "cluster") == 0) {
-        cluster = true;
-    } else if (strcmp(argv[1], "uniform") == 0) {
-        cluster = false;
-    } else {
+        cluster_count = 0;
+    } else if (strcmp(argv[1], "uniform") != 0) {
         fprintf(stderr, "ERROR: unrecognised mode: %s\n", argv[1]);
         exit(1);
     }
 
     // parse seed and pair_count args
-    seed = atoi(argv[2]);
-    pair_count = atoi(argv[3]);
+    seed = atoll(argv[2]);
+    pair_count = atoll(argv[3]);
 
-    // set seed to PRNG
+    // for breaking up into 64 clusters
+    u64 cluster_count_max = 1 + pair_count / 64;
+
+    // set PRNG seed
     srand(seed);
 
     // open files to be written to
     FILE *coordinates = fopen("haversine_coordinates.json", "wb");
     FILE *haversines = fopen("haversines.f64", "wb");
 
-    fprintf(coordinates, "{ \"pairs\": [");
+    f64 sum = 0;
+
+    fprintf(coordinates, "{ \"pairs\": [\n");
 
     // generate n_coords and compute their haversine
     for (int i = 0; i < pair_count; i++) {
         f64 x0, y0, x1, y1;
 
-        // generate coordinate pair
-        if (cluster) {
-            return 0;
-        } else {
-            x0 = random_f64(X_MIN, X_MAX);
-            y0 = random_f64(Y_MIN, Y_MAX);
-            x1 = random_f64(X_MIN, X_MAX);
-            y1 = random_f64(Y_MIN, Y_MAX);
+        if (cluster_count-- == 0) {
+            cluster_count = cluster_count_max;
+            x_centre = random_f64(-X_MAX, X_MAX);
+            y_centre = random_f64(-Y_MAX, Y_MAX);
+            x_radius = random_f64(0, X_MAX);
+            y_radius = random_f64(0, Y_MAX);
         }
+        x0 = random_degree(x_centre, x_radius);
+        y0 = random_degree(y_centre, y_radius);
+        x1 = random_degree(x_centre, x_radius);
+        y1 = random_degree(y_centre, y_radius);
+
 
         // write coordinates to coordinates file
-        fprintf(coordinates, "{ \"x0\":%.16f, \"y0\":%.16f, \"x1\":%.16f, \"y1\":%.16f }", x0, y0, x1, y1);
-        if (i + 1 < pair_count) fprintf(coordinates, ",");
+        fprintf(coordinates, "\t{ \"x0\": %.16f, \"y0\": %.16f, \"x1\": %.16f, \"y1\": %.16f }", x0, y0, x1, y1);
+        fprintf(coordinates, "%s", i + 1 == pair_count ? "\n" : ",\n");
 
         // compute haversine
         f64 hav = haversine(x0, y0, x1, y1, EARTH_RADIUS);
 
+        sum += hav;
+
         // write haversine to haversine file
         fwrite(&hav, sizeof(hav), 1, haversines);
-
     }
 
     fprintf(coordinates, "]}");
+
+    fprintf(stdout, "Method: %s\n", argv[1]);
+    fprintf(stdout, "Random seed: %llu\n", seed);
+    fprintf(stdout, "Pair count: %llu\n", pair_count);
+    fprintf(stdout, "Expected sum: %.16f\n", sum / (f64)pair_count);
 
     fclose(coordinates);
     fclose(haversines);
--- part2/haversine.h
+++ part2/haversine.h
@@ -9,11 +9,13 @@
 #define EARTH_RADIUS 6372.8
 
 #define X_MAX 180
-#define X_MIN -180
 #define Y_MAX 90
-#define Y_MIN -90
+
+#define min(a, b) (a) <= (b) ? (a) : (b)
+#define max(a, b) (a) >= (b) ? (a) : (b)
 
 typedef int32_t i32;
+typedef int64_t i64;
 typedef uint32_t u32;
 typedef uint64_t u64;
 typedef double f64;
