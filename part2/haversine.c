90ff559588bbb4e603e5e242a4e0461087144b9a
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2024-02-28 17:29:22 +1100

    set up tokeniser and parser

--- part2/haversine.c
+++ part2/haversine.c
@@ -1,7 +1,45 @@
 #include "haversine.h"
 
+#define MAX_IDENT 64            // max allowed length for an identifier in JSON
+#define MIN_JSON_PAIR_SIZE 24   // min 6 bytes (e.g. '"x0":0') * 4 coordinates == 24 bytes min
+
+typedef struct {
+    size_t count;
+    u8 *data;
+} buffer;
+
+typedef enum {
+    TOKEN_NONE,
+
+    TOKEN_LBRACE,
+    TOKEN_RBRACE,
+    TOKEN_IDENTIFIER,
+    TOKEN_FLOAT,
+    TOKEN_COMMA,
+    TOKEN_COLON,
+    TOKEN_LBRACKET,
+    TOKEN_RBRACKET,
+
+    TOKEN_COUNT
+} TokenType;
+
+typedef struct {
+    TokenType token_type;
+    union {
+        f64 number;
+        char identifier[MAX_IDENT];
+    };
+} Token;
+
+typedef struct {
+    f64 x0, y0;
+    f64 x1, y1;
+} Pair;
+
 buffer load_json(char *);
 Token *tokenize(buffer);
+
+// =================================== Haversine Formula ===================================//
 
 f64 square(f64 a) {
     f64 res = (a*a);
@@ -32,21 +70,73 @@ f64 haversine(f64 x0, f64 y0, f64 x1, f64 y1, f64 earth_radius) {
     return res;
 }
 
-i32 main(i32 argc, char *argv[]) {
+// ======================================= Tokenizer =======================================//
+
+char *curr_byte;    // pointer to current byte in json input
+Token token = {};   // current token
+
+void grab_identifier() {
+
+}
+
+void grab_number() {
+
+}
+
+void next_token() {
+    while (*curr_byte == ' ') {
+        curr_byte++;
+    }
+
+    char c = *curr_byte;
+
+    switch (c) {
+        case '{':
+            token.token_type = TOKEN_LBRACE;
+            break;
+        case '}':
+            token.token_type = TOKEN_RBRACE;
+            break;
+        case '\"':
+            grab_identifier();
+            break;
+        default:
+            if (c == '-' || isdigit(c)) {
+                grab_number();
+            }
+            break;
+    }
+}
+
+// ======================================== Parser =========================================//
+
+void parse_haversine_pairs(buffer input_json, Pair *pairs) {
+    curr_byte = (char *)input_json.data;
+    next_token();
+    if (token.token_type != TOKEN_LBRACE) {
+        fprintf(stderr, "PARSING ERROR: JSON input must begin with '{'\n");
+    }
+
+}
+
+// ===================================== Main Routine ======================================//
+
+int main(int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "USAGE: %s [coordinate_pairs.json]\n", argv[0]);
         exit(1);
     }
 
     // open json file > determine size of file > allocate memory for data > and read file into memory
+    char *filename = argv[1];
     buffer input_json = {};
     FILE *file;
-    if ((file = fopen(argv[0], "rb")) == NULL) {
-        fprintf(stderr, "ERROR: unable to open \"%s\"\n", argv[0]);
+    if ((file = fopen(filename, "rb")) == NULL) {
+        fprintf(stderr, "ERROR: unable to open \"%s\"\n", filename);
         exit(1);
     }
     struct stat file_stat;
-    stat(argv[0], &file_stat);
+    stat(filename, &file_stat);
     size_t count = file_stat.st_size;
     if ((input_json.data = (u8 *) malloc(count)) == NULL) {
         fprintf(stderr, "ERROR: unable to allocate %lu bytes\n", count);
@@ -54,10 +144,36 @@ i32 main(i32 argc, char *argv[]) {
     }
     input_json.count = count;
     if ((fread(input_json.data, input_json.count, 1, file)) != 1) {
-        fprintf(stderr, "ERROR: unable to read \"%s\"\n", argv[0]);
+        fprintf(stderr, "ERROR: unable to read \"%s\"\n", filename);
         free(input_json.data);
         exit(1);
     }
     fclose(file);
 
+    // allocate memory for haversine pairs (just estimate size based on input_json size)
+    buffer haversine_pairs = {};
+    u64 max_pair_count = input_json.count / MIN_JSON_PAIR_SIZE;
+    if (max_pair_count == 0) {
+        fprintf(stderr, "ERROR: malformed JSON input\n");
+        exit(1);
+    }
+    if ((haversine_pairs.data = (u8 *) malloc(max_pair_count * sizeof(Pair))) == NULL) {
+        fprintf(stderr, "ERROR: unable to allocate %lu bytes\n", haversine_pairs.count);
+        exit(1);
+    }
+    haversine_pairs.count = max_pair_count * sizeof(Pair);
+
+    // cast u8 array to Pair array
+    Pair *pairs = (Pair *)haversine_pairs.data;
+
+    // parse input JSON into haversine pairs
+    parse_haversine_pairs(input_json, pairs);
+
+    // TODO: sum haversine distances
+
+    // free allocated memory
+    free(haversine_pairs.data);
+    free(input_json.data);
+
+    return 0;
 }
--- part2/haversine.h
+++ part2/haversine.h
@@ -6,50 +6,21 @@
 #include <math.h>
 #include <assert.h>
 #include <sys/stat.h>
+#include <ctype.h>
 
 #define EARTH_RADIUS 6372.8
 #define X_MAX 180
 #define Y_MAX 90
 
-#define MAX_IDENT 64
+
 
 #define min(a, b) (a) <= (b) ? (a) : (b)
 #define max(a, b) (a) >= (b) ? (a) : (b)
 
-typedef int8_t u8;
+typedef uint8_t u8;
 typedef int32_t i32;
 typedef uint32_t u32;
 typedef int64_t i64;
 typedef uint64_t u64;
 
 typedef double f64;
-
-typedef struct {
-    size_t count;
-    u8 *data;
-} buffer;
-
-typedef enum {
-    TOKEN_NONE,
-
-    TOKEN_LBRACE,
-    TOKEN_RBRACE,
-    TOKEN_IDENTIFIER,
-    TOKEN_FLOAT,
-    TOKEN_COMMA,
-    TOKEN_COLON,
-    TOKEN_LBRACKET,
-    TOKEN_RBRACKET,
-
-    TOKEN_COUNT
-} TokenType;
-
-typedef struct {
-    TokenType token_type;
-    buffer value;
-} Token;
-
-typedef struct {
-    f64 x0, y0;
-    f64 x1, y1;
-} Pair;
