47b4541ee12b4ac133f8fb3eea3ad5f8f3669371
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2024-03-02 20:10:50 +1100

    add clock functions and reference solution for part2

---
+++ part2/haversine_clock.c
@@ -0,0 +1,62 @@
+#ifndef PERF_AWARE_HAVERSINE_H
+#include "haversine.h"
+#endif
+
+#include <sys/time.h>
+
+u64 get_os_time_freq(void) {
+    return 1000000;
+}
+
+u64 read_os_timer(void) {
+    struct timeval value;
+    gettimeofday(&value, 0);
+
+    u64 result = get_os_time_freq()*(u64)value.tv_sec + (u64)value.tv_usec;
+    return result;
+}
+
+u64 read_cpu_timer(void) {
+    u64 value;
+    asm("isb; mrs %0, CNTVCT_EL0" : "=r"(value));
+    printf("CNTVCT_EL0 = 0x%016llX\n", value);
+    return value;
+}
+
+int main(int argc, char *argv[]) {
+    u64 MillisecondsToWait = 1000;
+    if(argc == 2)
+    {
+        MillisecondsToWait = atol(argv[1]);
+    }
+
+    u64 OSFreq = get_os_time_freq();
+    printf("    OS Freq: %llu (reported)\n", OSFreq);
+
+    u64 CPUStart = read_cpu_timer();
+    u64 OSStart = read_os_timer();
+    u64 OSEnd = 0;
+    u64 OSElapsed = 0;
+    u64 OSWaitTime = OSFreq * MillisecondsToWait / 1000;
+    while(OSElapsed < OSWaitTime)
+    {
+        OSEnd = read_os_timer();
+        OSElapsed = OSEnd - OSStart;
+    }
+
+    u64 CPUEnd = read_cpu_timer();
+    u64 CPUElapsed = CPUEnd - CPUStart;
+    u64 CPUFreq = 0;
+    if(OSElapsed)
+    {
+        CPUFreq = OSFreq * CPUElapsed / OSElapsed;
+    }
+
+    printf("   OS Timer: %llu -> %llu = %llu elapsed\n", OSStart, OSEnd, OSElapsed);
+    printf(" OS Seconds: %.4f\n", (f64)OSElapsed/(f64)OSFreq);
+
+    printf("  CPU Timer: %llu -> %llu = %llu elapsed\n", CPUStart, CPUEnd, CPUElapsed);
+    printf("   CPU Freq: %llu (guessed)\n", CPUFreq);
+
+    return 0;
+}
